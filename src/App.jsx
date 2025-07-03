27aa9b5e8c144b559b79b19973fe8b46477b68c3
Daddycalling-u <ashadhossain666@gmail.com>
2025-07-03 19:05:36 +0530

    Temp rename to help casing on Windows

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import AuthPage from '@pages/AuthPage';
 import IndexPage from '@pages/Index';
 import PoemPage from '@pages/PoemPage';
 
-import { auth, db } from './firebase/firebaseconfig';
+import { auth, db } from './firebase/firebaseConfig';
 
 function App() {
   const [poems, setPoems] = useState([]);
